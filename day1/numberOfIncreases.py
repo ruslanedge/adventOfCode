1c2503dcfd2564a9e114d7944e129f6995c6a8ac
Ruslan <ruslanedge0123@gmail.com>
2021-12-07 17:06:40 -0600

    Fixed calculateIncrease to produce the right value

--- day1/numberOfIncreases.py
+++ day1/numberOfIncreases.py
@@ -1,23 +1,25 @@
 def calculateIncrease(valueArray):
     increaseCount = 0
-    last = dataArray[0]
-    for item in dataArray:
-        if item != dataArray[0] and item > last:
+    last = valueArray[0]
+    for item in valueArray:
+        if item > last:
             increaseCount += 1
         last = item
     return increaseCount
 
 inputFile = open("input.txt", "r")
 dataArray = []
+
 windowArray = []
 for line in inputFile.readlines():
     dataArray.append(int(line.replace("\n", "")))
 
 
 for i in range(len(dataArray)):
-    if i+2 < len(dataArray):
+    if i+2 <= len(dataArray)-1:
         windowArray.append(dataArray[i] + dataArray[i+1] + dataArray[i+2])
 
 
 print(calculateIncrease(dataArray))
 print(calculateIncrease(windowArray))
+
