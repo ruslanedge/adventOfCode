73ee1fe59c00a6637a99d06608f18f87a44bf0b9
Ruslan <ruslanedge0123@gmail.com>
2021-12-07 16:50:30 -0600

    Wrote code for sliding window calculations Moved calculation loop into function

--- day1/numberOfIncreases.py
+++ day1/numberOfIncreases.py
@@ -1,13 +1,23 @@
+def calculateIncrease(valueArray):
+    increaseCount = 0
+    last = dataArray[0]
+    for item in dataArray:
+        if item != dataArray[0] and item > last:
+            increaseCount += 1
+        last = item
+    return increaseCount
+
 inputFile = open("input.txt", "r")
 dataArray = []
+windowArray = []
 for line in inputFile.readlines():
     dataArray.append(int(line.replace("\n", "")))
 
-increaseCount = 0
-last = dataArray[0]
-for item in dataArray:
-    if item != dataArray[0] and item > last:
-        increaseCount += 1
-    last = item
 
-print(increaseCount)
+for i in range(len(dataArray)):
+    if i+2 < len(dataArray):
+        windowArray.append(dataArray[i] + dataArray[i+1] + dataArray[i+2])
+
+
+print(calculateIncrease(dataArray))
+print(calculateIncrease(windowArray))
